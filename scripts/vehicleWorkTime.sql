18604c1bbcb53432a4d689eaadaf6fb644648089
Janrupf <business.janrupf@gmail.com>
2021-05-27 23:01:10 +0200

    Seed vehicle work time

---
+++ scripts/vehicleWorkTime.sql
@@ -0,0 +1,3 @@
+INSERT INTO VehicleWorkTime (StartTime, EndTime) VALUES ('06:00:00', '12:00:00');
+INSERT INTO VehicleWorkTime (StartTime, EndTime) VALUES ('10:00:00', '18:00:00');
+INSERT INTO VehicleWorkTime (StartTime, EndTime) VALUES ('14:00:00', '22:00:00');
