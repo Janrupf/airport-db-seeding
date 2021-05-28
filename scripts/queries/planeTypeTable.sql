3a73e153d8fc5c7ade984626750dfece1b5d4752
Jonas <DbPWdW@GHitub359>
2021-05-28 14:56:11 +0200

    some more queries - mostly sync between multiple devices

--- scripts/queries/planeTypeTable.sql
+++ scripts/queries/planeTypeTable.sql
@@ -8,3 +8,9 @@ ORDER BY Name;
     --  todo some more stuff here:
     --      - airlines operating plane types
     --      - passenger distribution
+
+    --  print the operating plane types per airline
+SELECT A.Name, CONCAT_WS(', ', PT.Name) FROM Airline A
+JOIN Flight F on A.Callsign = F.AirlineCallsign
+JOIN PlaneType PT on F.PlaneType = PT.ID
+GROUP BY A.Callsign;
